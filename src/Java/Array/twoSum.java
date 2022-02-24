6d6f78cbbc2b7991a7fbcafb5b4c6f1ad73294b3
shubham wagh <shubham.wagh77@gmail.com>
2022-02-24 21:17:53 +0530

    two sum changes

--- src/Java/Array/twoSum.java
+++ src/Java/Array/twoSum.java
@@ -16,18 +16,19 @@ public class twoSum
 
 
     }
-    public static int[] calculate(int[] nums, int target) {
-     
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(target==nums[j]+nums[i]){
-                    System.out.println(i+" "+j);
-                    return new int[] { i, j };
-                
-                }
-                
+    public static int[] calculate(int[] numbers, int target) {
+        int[] result = new int[2];
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i;
+                result[0] = map.get(target - numbers[i]);
+                return result;
             }
-        }throw new IllegalArgumentException("No two sum solution");
+            map.put(numbers[i], i);
+        }
+        return result;
             
     }
 
