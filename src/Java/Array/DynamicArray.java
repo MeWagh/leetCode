5f460249bb056ef65097791e9abebe2d4fb500d8
shubham wagh <shubham.wagh77@gmail.com>
2022-02-26 19:56:49 +0530

    Dynamic Array implementation

---
+++ src/Java/Array/DynamicArray.java
@@ -0,0 +1,114 @@
+// mutable array with automatic resizing
+
+
+public class DynamicArray 
+{
+
+    private int _array[];
+    private int _count;
+    private int _size;
+    public DynamicArray() 
+    {
+        _array = new int[1];
+        _count= 0;
+        _size = 1;
+    }
+    public void Add(int data)
+    {
+        if(_count == _size)
+        {
+            GrowSize();
+        }
+        _array[_count] = data;
+        _count++;
+
+    }
+    public void ShrinkSize()
+    {
+        int temp[] =new int[_count];
+        for(int i=0;i<_count;i++)
+        {
+            temp[i] = _array[i]; 
+        }
+        _size=_count;
+        _array = temp;
+    }
+    public void AddAt(int index,int data)
+    {
+        if(_count == _size)
+        {
+            GrowSize();
+        }
+        for(int i= _count-1; i >=index;i--)
+        {
+            _array[i + 1] = _array[i];
+        }
+        _array[index] = data;
+        _count++;
+    }
+    public void removeAt(int index)
+    {
+       
+        for(int i= index ; i < _count;i++)
+        {
+            _array[i] = _array[i+1];
+        }
+        _array[_count - 1] = 0;
+        _count--;
+    }
+    private void GrowSize()
+    {
+        int temp[] =new int[_size * 2];
+        for(int i=0;i<_size;i++)
+        {
+            temp[i] = _array[i]; 
+        }
+        _size= _size *2;
+        _array = temp;
+    }
+    public static void main(String[] args)
+    {
+        DynamicArray da = new DynamicArray();
+  
+        // add 9 elements in array
+        da.Add(1);
+        da.Add(2);
+        da.Add(3);
+        da.Add(4);
+        da.Add(5);
+        da.Add(6);
+        da.Add(7);
+        da.Add(8);
+        da.Add(9);
+  
+        da.PrintArray(da);
+        da.ShrinkSize();
+        da.PrintArray(da);
+
+         // add an element at index 1
+         da.AddAt(1, 22);
+         da.PrintArray(da);
+         da.removeAt(5);
+         da.PrintArray(da);
+         da.ShrinkSize();
+         da.PrintArray(da);
+
+    }
+    public void PrintArray(DynamicArray da)
+    {
+        System.out.println("Elements of array:");
+        for (int i = 0; i < da._size; i++) {
+            System.out.print(da._array[i] + " ");
+        }
+
+    }
+  
+
+
+}
+// Elements of array:
+// 1 2 3 4 5 6 7 8 9 0 0 0 0 0 0 0 Elements of array:
+// 1 2 3 4 5 6 7 8 9 Elements of array:
+// 1 22 2 3 4 5 6 7 8 9 0 0 0 0 0 0 0 0 Elements of array:
+// 1 22 2 3 4 6 7 8 9 0 0 0 0 0 0 0 0 0 Elements of array:
+// 1 22 2 3 4 6 7 8 9
